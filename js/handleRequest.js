ae4772bb74c7825930005701b6264def1f74c7ca
vila994 <vila994@gmail.com>
2018-07-05 17:13:33 +0200

    first commit

---
+++ js/handleRequest.js
@@ -0,0 +1,14 @@
+function inviaCase(){
+
+	var nome = $('#nomeId').val();
+	var cognome = $('#cognomeId').val();
+	var email = $('#emailId').val();
+	var argomento = $('#argomentoId').find('option:selected').text();
+	var note = $('#noteId').val();
+
+
+	console.log(argomento);
+
+
+
+}
